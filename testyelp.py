6805cdf3377fb9f611aa47240912a25d14d393a0
Hackbright Student <info@hackbrightacademy.com>
2015-08-04 16:56:48 -0700

    search working in testyelp.py

--- testyelp.py
+++ testyelp.py
@@ -8,5 +8,33 @@ yelp_api = yelp.Api(
     access_token_secret=os.environ['YELP_ACCESS_TOKEN_SECRET'])
 
 
-business = yelp_api.GetBusiness('post-no-bills-brooklyn')
-print business.name
+
+def yelp_to_salon_list_SF(search_term, yelp_ids_dict):
+    search_results = yelp_api.Search(term=search_term, location="San Francisco, CA", radius_filter=6000)
+    total_results = search_results.total
+
+    offset = total_results % 20 - 1
+
+    while offset < total_results:
+        search_results_expanded = yelp_api.Search(term=search_term, offset=offset, location="San Francisco, CA", radius_filter=6000)
+        for business in search_results_expanded.businesses:
+            if business.name not in yelp_ids_dict:
+                yelp_ids_dict[business.name] = business.id
+                print business.name, business.id
+        offset = offset + 20
+
+
+    return yelp_ids_dict
+
+yelp_ids_empty = {}
+yelp_ids_manicures = yelp_to_salon_list_SF('manicure', yelp_ids_empty)
+print '1'
+print len(yelp_ids_manicures)
+
+# yelp_ids_nail_salons1 = yelp_to_salon_list_SF('nail salon', yelp_ids_empty)
+# print '2'
+# print len(yelp_ids_nail_salons1)
+
+yelp_ids_nail_salons = yelp_to_salon_list_SF('nail salon', yelp_ids_manicures)
+print '3'
+print len(yelp_ids_nail_salons)
